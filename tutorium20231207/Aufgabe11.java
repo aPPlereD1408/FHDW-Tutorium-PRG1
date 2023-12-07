7dc800f1de5ce2603010ef53f29340f10380a97c
Gereon Krümmling <134088093+aPPlereD1408@users.noreply.github.com>
2023-12-07 22:12:31 +0100

    Add files via upload

---
+++ tutorium20231207/Aufgabe11.java
@@ -0,0 +1,41 @@
+package de.fhdw.prg1.bfwc423a.kru.tutorium20231205.aufgaben;
+
+public class Aufgabe11 {
+    public static void main(String[] args) {
+        triangle(2);
+    }
+
+    static void triangle(int n) {
+        System.out.println("Dreieck der Größe " + n + ":\n");
+        if (n <= 0) {
+            System.out.println();
+        } else if (n == 1) {
+            System.out.println("*");
+        } else {
+            drawTriangle(n);
+        }
+    }
+
+    static void drawTriangle(int n) {
+        System.out.println("*"); // Zeile 1
+        for (int i = 0; i < n - 2; i++) {
+            drawLines(i + (i + 1));
+        }
+        drawBottomLine(n);
+    }
+
+    static void drawLines(int leerzeichen) {
+        System.out.print("*");
+        for (int j = 0; j < leerzeichen; j++) {
+            System.out.print(" ");
+        }
+        System.out.print("*\n");
+    }
+
+    static void drawBottomLine(int n) {
+        for (int i = 0; i < n + (n - 1); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
