789338f2e6bd3dfb7d3614aef7bc202e3dfaf9f1
Gereon Krümmling <134088093+aPPlereD1408@users.noreply.github.com>
2023-12-19 15:29:53 +0100

    ListNode.java
    ListNode für Liste

---
+++ ListNode.java
@@ -0,0 +1,20 @@
+public class ListNode {
+    int mContent;
+    ListNode mSuccessor;
+
+    public int getContent() {
+        return mContent;
+    }
+
+    public void setContent(int content) {
+        mContent = content;
+    }
+
+    public ListNode getSuccessor() {
+        return mSuccessor;
+    }
+
+    public void setSuccessor(ListNode successor) {
+        mSuccessor = successor;
+    }
+}
